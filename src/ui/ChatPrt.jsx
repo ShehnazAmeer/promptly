d5ae1190d8d16dac342a649c1143ae740cb09def
ShehnazAmeer <48834996+ShehnazAmeer@users.noreply.github.com>
2026-03-30 21:43:49 +0500

    working on chat component

--- src/ui/ChatPrt.jsx
+++ src/ui/ChatPrt.jsx
@@ -1,7 +1,8 @@
 export default function ChatPrt() {
     return (
-        <div>
-            Chat
+        <div className="flex flex-col justify-center border border-red-500 h-screen w-full items-center">
+            <h3>Where do we start</h3>
+            <textarea className="bg-gray-700  w-[clamp(7rem,13vw,12rem)] text-[clamp(0.7rem,1vw,2rem)]"></textarea>
         </div>
     )
 }
